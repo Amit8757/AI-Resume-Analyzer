75db71d41bf502dda35856efde9daf9e2824aa22
Amit8757 <amitpathak826@gmail.com>
2026-02-05 20:39:50 +0530

    update base url

--- Client/src/services/authService.js
+++ Client/src/services/authService.js
@@ -4,7 +4,7 @@ import api from './api';
  * Register a new user
  */
 export const register = async (userData) => {
-    const response = await api.post('/api/auth/register', userData);
+    const response = await api.post('/auth/register', userData);
     if (response.data.success) {
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('user', JSON.stringify(response.data.user));
